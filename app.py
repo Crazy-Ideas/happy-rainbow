0dd8a5c3c63af9c5cc6a555b53d6f678fa41035f
nayan <nayan@crazyideas.co.in>
2021-08-09 11:16:41 +0530

    Home page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
 from flask import Flask
-from flask_login import LoginManager
+
+# from flask_login import LoginManager
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-cloud.json"
 
@@ -9,15 +10,12 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-login = LoginManager(app)
-login.login_view = "login"
-login.session_protection = "strong" if Config.CI_SECURITY else "basic"
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+# login = LoginManager(app)
+# login.login_view = "login"
+# login.session_protection = "strong" if Config.CI_SECURITY else "basic"
 
 if __name__ == '__main__':
     app.run()
+
+# noinspection PyUnresolvedReferences
+from routes import *
